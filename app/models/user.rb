762a852df4a76e14ba3f3f0cb712f09a360d7482
Karl Stolley <karl.stolley@gmail.com>
2015-03-31 11:31:01 -0500

    Created session controller by hand plus method to find or create a user based on the auth hash returned by Twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_many :ads
+
+  def self.find_or_create_from_auth_hash(hash)
+    if find_by_provider_and_uid(hash['provider'], hash['uid'])
+    else
+      create(username: hash['info']['nickname'], name: hash['info']['nickname'],location: hash['info']['location'], image: hash['info']['image'], provider: hash['provider'], uid: hash['uid'])
+    end
+  end
+
 end
