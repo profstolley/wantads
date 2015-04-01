a608a264213ed252f5cfa1ac331e637f29741227
Karl Stolley <karl.stolley@gmail.com>
2015-03-31 19:45:04 -0500

    Return a user if one is found; also fixed name reference in hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   has_many :ads
 
   def self.find_or_create_from_auth_hash(hash)
-    if find_by_provider_and_uid(hash['provider'], hash['uid'])
+    if @user = find_by_provider_and_uid(hash['provider'], hash['uid'])
+      @user
     else
-      create(username: hash['info']['nickname'], name: hash['info']['nickname'],location: hash['info']['location'], image: hash['info']['image'], provider: hash['provider'], uid: hash['uid'])
+      @user = create(username: hash['info']['nickname'], name: hash['info']['name'],location: hash['info']['location'], image: hash['info']['image'], provider: hash['provider'], uid: hash['uid'])
     end
   end
 
